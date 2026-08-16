350f79697436f9da16bb825328bce616d2eddea2
M Ali Iftikhar <139961208+aliiftikhar22@users.noreply.github.com>
2026-08-16 13:01:42 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
+import {
+  getFirestore,
+  collection,
+  addDoc,
+  getDocs,
+  query,
+  orderBy
+} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
 /*
   DAROOD PAK GOAL
   ------------------------------------------------------------
